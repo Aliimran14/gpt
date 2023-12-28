8c2eaeda60d2c8a3154ff93f1be886a9fefe5d79
Aliimran14 <130079806+Aliimran14@users.noreply.github.com>
2023-12-28 12:22:55 -0800

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -17,7 +17,7 @@ from streamlit_chat import message
 from sentence_transformers import SentenceTransformer
 import requests
 #openapi_key = st.secrets["OPENAI_API_KEY"]
-#hf_api_key = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+hf_api_key = HUGGINGFACEHUB_API_TOKEN
 hf_api_key = "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"
 # "with" notation
 def main():
